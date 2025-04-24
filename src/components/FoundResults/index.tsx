331e8646b8c4587572f3e0b7f56f1ac2e95cff70
Nimus-oes <sseo.code@gmail.com>
2025-04-24 15:18:51 +0900

    chore: update base structure

---
+++ src/components/FoundResults/index.tsx
@@ -0,0 +1,3 @@
+export default function FoundResults() {
+  return <div></div>;
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,19 @@
+import CountryTable from "../CountryTable";
+import FoundResults from "../FoundResults";
+import RegionFilters from "../RegionFilters";
+import SearchBar from "../SearchBar";
+import SortDropdown from "../SortDropdown";
+import StatusFilters from "../StatusFilters";
+
 export default function Main() {
-  return <div></div>;
+  return (
+    <main>
+      <FoundResults />
+      <SearchBar />
+      <SortDropdown />
+      <RegionFilters />
+      <StatusFilters />
+      <CountryTable />
+    </main>
+  );
 }
