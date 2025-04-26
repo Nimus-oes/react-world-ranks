7a514284b5e81a075bbebcc850d766155412e867
Nimus-oes <sseo.code@gmail.com>
2025-04-26 10:13:02 +0900

    feature: render country data into a table

--- src/components/CountryTable/index.tsx
+++ src/components/CountryTable/index.tsx
@@ -28,5 +28,36 @@ export default function CountryTable() {
       .then((data) => setCountries(data));
   }, []);
 
-  return <div>Country Table</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Flag</th>
+          <th>Name</th>
+          <th>Population</th>
+          <th>Area</th>
+          <th>Region</th>
+        </tr>
+      </thead>
+      <tbody>
+        {countries.map((country) => (
+          <tr>
+            <td>
+              <div>
+                <img
+                  src={country.flags.svg}
+                  alt={country.flags.alt}
+                  width="50px"
+                />
+              </div>
+            </td>
+            <td>{country.name.common}</td>
+            <td>{country.population}</td>
+            <td>{country.area}</td>
+            <td>{country.region}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
