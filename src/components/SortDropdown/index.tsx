97925ddd80123d11b8c8c4046591d4913e464369
Nimus-oes <sseo.code@gmail.com>
2025-04-29 16:00:57 +0900

    chore: add title to sort by menu

--- src/components/SortDropdown/index.tsx
+++ src/components/SortDropdown/index.tsx
@@ -2,21 +2,24 @@ import type { FilterType, FilterProp } from "../Main";
 
 export default function SortDropdown({ filters, setFilters }: FilterProp) {
   return (
-    <select
-      name=""
-      id=""
-      value={filters.sortBy}
-      onChange={(e) =>
-        setFilters((prevFilters) => ({
-          ...prevFilters,
-          sortBy: e.target.value as FilterType["sortBy"],
-        }))
-      }
-    >
-      <option value="Name">Name</option>
-      <option value="Population">Population</option>
-      <option value="Area">Area</option>
-      <option value="Region">Region</option>
-    </select>
+    <>
+      <h3>Sort by</h3>
+      <select
+        name=""
+        id=""
+        value={filters.sortBy}
+        onChange={(e) =>
+          setFilters((prevFilters) => ({
+            ...prevFilters,
+            sortBy: e.target.value as FilterType["sortBy"],
+          }))
+        }
+      >
+        <option value="Name">Name</option>
+        <option value="Population">Population</option>
+        <option value="Area">Area</option>
+        <option value="Region">Region</option>
+      </select>
+    </>
   );
 }
