af3e6dd6c8d13a3bc97b219669800f72936f7b53
Nimus-oes <sseo.code@gmail.com>
2025-05-04 10:07:31 +0900

    Clear previous codes and start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -8,8 +6,6 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Header />
-      <Main />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
--- src/components/FoundResults/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FilteredCountriesProp } from "../Main";
-
-export default function FoundResults({
-  filteredCountries,
-}: FilteredCountriesProp) {
-  const numsOfCountries = filteredCountries.length;
-  return <div>Found {numsOfCountries} Results</div>;
-}
