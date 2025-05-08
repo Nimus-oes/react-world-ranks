5876aae73b471a963bae22a54fc4c41aa1bd163e
Nimus-oes <sseo.code@gmail.com>
2025-05-08 14:20:34 +0900

    Add responsive hero images

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import styles from "./Header.module.css";
-import logo from "../../assets/hero-image.jpg";
+import smLogo from "../../assets/hero-sm.png";
+import lgLogo from "../../assets/hero-lg.png";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div>
-        <img src={logo} alt="" />
-      </div>
+      <picture>
+        <source media="(min-width: 1024px)" srcSet={lgLogo} />
+        <img src={smLogo} />
+      </picture>
     </header>
   );
 }
