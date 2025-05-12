aac15a348bd1253ec8d031128cd779fa36028f83
Nimus-oes <sseo.code@gmail.com>
2025-05-12 15:44:18 +0900

    Replace select component with a Radix component

--- src/components/SortBy/index.tsx
+++ src/components/SortBy/index.tsx
@@ -2,29 +2,43 @@ import { useTranslation } from "react-i18next";
 import { SORT_CATEGORIES } from "./constants";
 import { FilterProp, SorterType } from "../../types/models";
 import styles from "./SortBy.module.css";
+import { Select } from "radix-ui";
 
 export default function SortBy({ filters, setFilters }: FilterProp) {
   const { t } = useTranslation();
-  const handleChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    const selected = e.target.value as SorterType;
-    setFilters((prevFilters) => ({ ...prevFilters, sorter: selected }));
+  const handleChange = (value: string) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      sorter: value as SorterType,
+    }));
   };
+
   return (
     <div>
       <h3 className={styles.title}>{t("sorter_title")}</h3>
-      <select
-        name=""
-        id=""
-        value={filters.sorter}
-        onChange={handleChange}
-        className={styles.dropdown}
-      >
-        {SORT_CATEGORIES.map((cat) => (
-          <option key={cat} value={cat}>
-            {t(`sorter_options.${cat}`)}
-          </option>
-        ))}
-      </select>
+      <Select.Root value={filters.sorter} onValueChange={handleChange}>
+        <Select.Trigger className={styles.trigger}>
+          <Select.Value />
+          <Select.Icon />
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Content
+            className={styles.content}
+            position="popper"
+            sideOffset={4}
+          >
+            <Select.Viewport>
+              {SORT_CATEGORIES.map((cat) => (
+                <Select.Item key={cat} value={cat} className={styles.item}>
+                  <Select.ItemText>
+                    {t(`sorter_options.${cat}`)}
+                  </Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
     </div>
   );
 }
